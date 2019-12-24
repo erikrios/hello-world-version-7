2586e5922dd6bef085b37d714a2a6ddde3bc1d29
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2019-12-24 22:14:48 +0700

    Initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Demo Flutter'),
+        ),
+        body: Material(
+          color: Colors.blueAccent,
+          child: Center(
+            child: Text(
+              'Hello, World!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 45.0,
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+}
